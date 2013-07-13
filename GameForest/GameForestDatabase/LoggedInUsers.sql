d64dd440300f0250f59a1fd74cad1902efaed6ec
Michael Ong <michaelong18@outlook.com>
2013-07-13 14:54:00 +0800

    Added the MySQL database file.

--- GameForest/GameForestDatabase/LoggedInUsers.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[LoggedInUsers]
-(
-    [SessionId] NCHAR(10) NOT NULL , 
-    [UserId] UNIQUEIDENTIFIER NOT NULL , 
-    [LastHeartbeat] DATETIME NOT NULL, 
-    PRIMARY KEY ([SessionId]), 
-    CONSTRAINT [UserIdFromRegisteredUsers] FOREIGN KEY ([UserId]) REFERENCES [RegisteredUsers]([UserId])
-)
--- GameForest/GameForestDatabase/RegisteredUsers.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[RegisteredUsers]
-(
-    [UserId] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY, 
-    [UserName] VARBINARY(50) NOT NULL, 
-    [Password] VARCHAR(50) NOT NULL, 
-    [FirstName] VARCHAR(50) NULL, 
-    [LastName] VARCHAR(50) NULL, 
-    [Description] VARCHAR(MAX) NULL
-)
