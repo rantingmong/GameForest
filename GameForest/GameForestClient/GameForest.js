ab9ef01ab3b38a204f3baf7e3eadced5edf5caa5
Michael Ong <michaelong18@outlook.com>
2013-08-10 17:42:55 +0800

    Added missing files

---
+++ GameForest/GameForestClient/GameForest.js
@@ -0,0 +1,54 @@
+﻿var GFX_SUBJ_INIT_CONNECTION = "GFX_INIT_CONNECTION";
+var GFX_SUBJ_STOP_CONNECTION = "GFX_STOP_CONNECTION";
+
+var GFXUserInfo = (function () {
+    function GFXUserInfo() {
+    }
+    return GFXUserInfo;
+})();
+
+var GFXClientCore = (function () {
+    function GFXClientCore() {
+        this.connectionStringGot = false;
+        this.webSocketConnection = new WebSocket("ws://localhost:1193");
+
+        this.sesssionId = "";
+        this.connectionId = "";
+    }
+    GFXClientCore.prototype.createWebSocket = function () {
+        this.webSocketConnection.onopen = this.webSocketOnOpen;
+        this.webSocketConnection.onclose = this.webSocketOnClose;
+        this.webSocketConnection.onmessage = this.webSocketOnMessage;
+    };
+
+    GFXClientCore.prototype.webSocketOnOpen = function () {
+    };
+    GFXClientCore.prototype.webSocketOnClose = function () {
+    };
+    GFXClientCore.prototype.webSocketOnMessage = function (message) {
+        var parsedMessage = JSON.parse(message.data);
+
+        if (this.connectionStringGot == false) {
+            this.connectionId = parsedMessage;
+            this.connectionStringGot = true;
+        } else {
+            switch (parsedMessage.Subject) {
+            }
+        }
+    };
+
+    GFXClientCore.prototype.login = function (username, password) {
+    };
+
+    GFXClientCore.prototype.logout = function () {
+    };
+
+    GFXClientCore.prototype.getUserInfo = function (userNameOrUserId) {
+        return new GFXUserInfo();
+    };
+
+    GFXClientCore.prototype.setUserInfo = function (userInfo) {
+    };
+    return GFXClientCore;
+})();
+//@ sourceMappingURL=GameForest.js.map
---
+++ GameForest/GameForestClient/GameForest.ts
@@ -0,0 +1,80 @@
+﻿var GFX_SUBJ_INIT_CONNECTION = "GFX_INIT_CONNECTION";
+var GFX_SUBJ_STOP_CONNECTION = "GFX_STOP_CONNECTION";
+
+class GFXUserInfo {
+
+    public firstName:       string;
+    public lastName:        string;
+
+    public userName:        string;
+    public description:     string;
+}
+
+class GFXClientCore {
+
+    private webSocketConnection:    WebSocket;
+
+    private sesssionId:             string;
+    private connectionId:           string;
+
+    private connectionStringGot:    boolean;
+
+    constructor         () {
+
+        this.connectionStringGot = false;
+        this.webSocketConnection = new WebSocket("ws://localhost:1193");
+
+        this.sesssionId     = "";
+        this.connectionId   = "";
+    }
+
+    createWebSocket     () {
+
+        this.webSocketConnection.onopen     = this.webSocketOnOpen;
+        this.webSocketConnection.onclose    = this.webSocketOnClose;
+        this.webSocketConnection.onmessage  = this.webSocketOnMessage;
+    }
+
+    webSocketOnOpen     () {
+
+    }
+    webSocketOnClose    () {
+
+    }
+    webSocketOnMessage  (message: MessageEvent) {
+        
+        var parsedMessage = JSON.parse(message.data);
+
+        if (this.connectionStringGot == false) {
+
+            this.connectionId           = parsedMessage;
+            this.connectionStringGot    = true;
+        }
+        else {
+
+            switch (parsedMessage.Subject) {
+
+            }
+        }
+    }
+
+    login               (username: string, password: string) {
+
+        
+    }
+
+    logout              () {
+
+
+    }
+
+    getUserInfo         (userNameOrUserId: string) {
+
+        return new GFXUserInfo();
+    }
+
+    setUserInfo         (userInfo: GFXUserInfo) {
+
+
+    }
+}
