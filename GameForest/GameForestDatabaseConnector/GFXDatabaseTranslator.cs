a21641f8227162b66d82d09119a36f14059bea16
Michael Ong <michaelong18@outlook.com>
2013-08-10 15:55:25 +0800

    Separated Database classes onto a separate project.

---
+++ GameForest/GameForestDatabaseConnector/GFXDatabaseTranslator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using MySql.Data.MySqlClient;
+
+namespace GameForestCore.Database
+{
+    public interface GFXDatabaseTranslator<T>
+    {
+        string              TableName       { get; }
+        IEnumerable<string> TableColumns    { get; }
+
+        IEnumerable<string> ToStringValues  (T data);
+        T                   ToNativeData    (MySqlDataReader reader);
+    }
+}
