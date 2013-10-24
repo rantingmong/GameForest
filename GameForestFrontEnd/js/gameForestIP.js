c238732ac2dc384261b3298f3a49881c0ece6ff0
Michael Ong <michaelong18@outlook.com>
2013-10-24 22:27:03 +0800

    Updated gameforestip.js

--- GameForestFrontEnd/js/gameForestIP.js
+++ GameForestFrontEnd/js/gameForestIP.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-var gameForestIP = "10.2.180.197";
-=======
-var gameForestIP = "localhost";
->>>>>>> 43f7964319730ac6efe14c244bc21c45be301dd4
+var gameForestIP = "game-forest.cloudapp.net";
